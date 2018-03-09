a64d5940505feb3cc77d192eee754c2e05ee849c
Andrew Meyers <ameyers.central@gmail.com>
2018-03-09 01:58:03 -0500

    Testing 1st Build

--- app/src/main/java/com/example/bluefletchandroidproject/MainActivity.java
+++ app/src/main/java/com/example/bluefletchandroidproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bluefletchandroidproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText Password;
     private TextView Info;
     private Button Login;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +26,36 @@ public class MainActivity extends AppCompatActivity {
         Info = (TextView)findViewById(R.id.tvInfo);
         Login = (Button)findViewById(R.id.btnLogin);
 
+        Info.setText("No. of attemps remaining: 5");
+
+        /* Setting up Listener for Login button */
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(),
+                         Password.getText().toString());
+            }
+
+        });
+
     }
 
     private void validate(String userName, String userPassword) {
-        if ((userName == "Admin") && (userPassword == "Admin@123")) {
+        if ((userName.equals("Admin")) && (userPassword.equals("Admin123"))) {
             /* Go to new Activity */
             Intent intent = new Intent(MainActivity.this, AppActivity.class);
+            startActivity(intent);
         }
+        else {
+            counter--;
+
+            Info.setText("No. of attempts remaining: " + String.valueOf(counter));
+
+            if (counter == 0) {
+                Login.setEnabled(false);
+            }
+
+        }
+
     }
 }
