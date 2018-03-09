0fe1c2cebba93b7761bec5da3e98836c4ef8f40b
Andrew Meyers <ameyers.central@gmail.com>
2018-03-09 01:30:18 -0500

    Created a new Empty Activity

--- app/src/main/java/com/example/bluefletchandroidproject/MainActivity.java
+++ app/src/main/java/com/example/bluefletchandroidproject/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.bluefletchandroidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+    }
+
+    private void validate(String userName, String userPassword) {
+        if ((userName == "Admin") && (userPassword == "Admin@123")) {
+            /* Go to new Activity */
+            Intent intent = new Intent(MainActivity.this, AppActivity.class);
+        }
     }
 }
